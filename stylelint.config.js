a5a5c54565c6e0dc43a2d6c693458f81ff2d757a
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-29 00:28:09 +1000

    test

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,8 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-  extends: ['stylelint-config-sass-guidelines', 'stylelint-config-prettier'],
-  rules: {
-    'selector-max-id': null,
-    'selector-no-qualifying-type': null,
-    'selector-class-pattern': null,
-    'declaration-block-single-line-max-declarations': null,
-  },
-  ignoreFiles: ['node_modules/*', 'dist/*'],
+  extends: [
+    'stylelint-config-recommended-scss',
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-prettier',
+  ],
 };
